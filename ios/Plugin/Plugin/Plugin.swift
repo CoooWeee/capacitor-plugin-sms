a93eecd9dc605c8a5f80b08112ec21bc416d1913
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-09-18 12:15:38 +0200

    Updated ios version

--- ios/Plugin/Plugin/Plugin.swift
+++ ios/Plugin/Plugin/Plugin.swift
@@ -9,9 +9,68 @@ import Capacitor
 public class SMSPlugin: CAPPlugin {
     
     @objc func echo(_ call: CAPPluginCall) {
+        print("[SMS Plugin Native iOS]: echo");
         let value = call.getString("value") ?? ""
         call.success([
             "value": value
         ])
     }
+    
+    @objc func sendSMS(_ call: CAPPluginCall) {
+        print("[SMS Plugin Native iOS]: sendSMS");
+        let value = call.getString("value") ?? ""
+        call.success([
+            "value": value
+        ])
+    }
+   
+    /*
+    func sendSMSText(phoneNumber: String) {
+        if (MFMessageComposeViewController.canSendText()) {
+            let controller = MFMessageComposeViewController()
+            controller.body = ""
+            controller.recipients = [phoneNumber]
+            controller.messageComposeDelegate = self
+            self.present(controller, animated: true, completion: nil)
+        }
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        //... handle sms screen actions
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = false
+    }
+     */
+    
+    /*
+    func sendSMS() {
+        
+        let messageVC = MFMessageComposeViewController()
+        
+        messageVC.body = "Enter a message";
+        messageVC.recipients = ["Enter tel-nr"]
+        messageVC.messageComposeDelegate = self;
+        
+        self.present(messageVC, animated: false, completion: nil)
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        switch (result.rawValue) {
+        case MessageComposeResult.cancelled.rawValue:
+            print("Message was cancelled")
+            self.dismiss(animated: true, completion: nil)
+        case MessageComposeResult.failed.rawValue:
+            print("Message failed")
+            self.dismiss(animated: true, completion: nil)
+        case MessageComposeResult.sent.rawValue:
+            print("Message was sent")
+            self.dismiss(animated: true, completion: nil)
+        default:
+            break;
+        }
+    }
+ */
 }
